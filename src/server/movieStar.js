7bf7a856e79b748284c85a7a2d4e5de47381b6f1
Karini Magalhaes <karini.magalhaes@maxmilhas.com.br>
2019-05-25 19:11:48 -0300

    Adicionado serviço de adicionar favorito e docker-compose do mongodb

--- src/server/movieStar.js
+++ src/server/movieStar.js
@@ -1,4 +1,9 @@
 import client from './src/client/client.js';
+import mongoose from 'mongoose';
+
+const bdConnection = () => {
+    mongoose.connect('mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb');
+}
 
 client.movieStar(getDetail(movieId), (error, movie) => {
     if(!error){
