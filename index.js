f71586746913d7fb242843bef3dfc040674cb9ab
Karini Magalhaes <karini.magalhaes@maxmilhas.com.br>
2019-06-15 14:31:57 -0300

    Alteração para call stream

--- index.js
+++ index.js
@@ -9,27 +9,9 @@ const searchMovieDbProvider = require("./src/requests-providers/moviedb-provider
 const PROTO_PATH = path.resolve(__dirname, "./src/protos/movies.proto");
 
 const searchMovie = async ctx => {
-
+    console.log(ctx.type);
     console.log("Request received.");
-    console.log("page: " + ctx.request.req.page);
-    let listMovies = await searchMovieDbProvider(ctx.request.req.movieName, ctx.request.req.page);
-    let movies = [];
-
-    for (let index = 0; index < listMovies.length; index++) {
-        movies.push({
-            vote_count: listMovies[index].vote_count,
-            vote_average: listMovies[index].vote_average,
-            title: listMovies[index].title,
-            popularity: listMovies[index].popularity,
-            original_language: listMovies[index].original_language,
-            original_title: listMovies[index].original_title,
-            overview: listMovies[index].overview,
-            release_date: listMovies[index].release_date,
-            id: listMovies[index].id
-        });
-        
-    };
-    ctx.res = { movies };
+    await searchMovieDbProvider(ctx.request.req.movieName, ctx.request.req.page, ctx.call);
 };
 
 const main = () => {
--- src/requests-providers/moviedb-provider.js
+++ src/requests-providers/moviedb-provider.js
@@ -15,8 +15,7 @@ let movies = [];
 let pages = 1;
 let movieDb;
 
-const searchMovieDbProvider = async (movieName, page) => { 
-    console.log("page req: " + page)
+const searchMovieDbProvider = async (movieName, page, stream) => { 
     try{
             let response = await request
             .get(`${MOVIE_BASE_URI}/search/movie`)
@@ -24,14 +23,32 @@ const searchMovieDbProvider = async (movieName, page) => {
             .query({query: movieName})
             .query({page: page})
             .send();
-        movies.push(response.body.results);
+        const listMovies = response.body.results;
+        const movies = [];
+        for (let index = 0; index < listMovies.length; index++) {
+            const movie = {
+                vote_count: listMovies[index].vote_count,
+                vote_average: listMovies[index].vote_average,
+                title: listMovies[index].title,
+                popularity: listMovies[index].popularity,
+                original_language: listMovies[index].original_language,
+                original_title: listMovies[index].original_title,
+                overview: listMovies[index].overview,
+                release_date: listMovies[index].release_date,
+                id: listMovies[index].id
+            };
+            movies.push(movie);
+            
+        }
+
+        stream.write({ movies });
         console.log("total pages: " + response.body.total_pages, "pages: " + pages)
         if(pages !== response.body.total_pages){
             pages++;
-            await searchMovieDbProvider(movieName, pages);
+            await searchMovieDbProvider(movieName, pages, stream);
         }
     } catch (error) {
-        console.error(error)
+        //console.error(error)
     }
 
     return _.flatten(movies);
