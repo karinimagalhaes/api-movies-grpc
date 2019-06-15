1d126cab0c196cfb8108b7e7c157c5fbc1fa46eb
Karini Magalhaes <karini.magalhaes@maxmilhas.com.br>
2019-06-15 17:24:17 -0300

    teste

--- src/requests-providers/moviedb-provider.js
+++ src/requests-providers/moviedb-provider.js
@@ -1,7 +1,3 @@
-// import request from 'superagent';
-// import * as _ from 'lodash';
-// import dotenv from 'dotenv';
-
 const request = require('superagent');
 var _ = require('lodash');
 const dotenv = require('dotenv');
@@ -16,39 +12,36 @@ let pages = 1;
 let movieDb;
 
 const searchMovieDbProvider = async (movieName, page, stream) => { 
-    try{
-            let response = await request
-            .get(`${MOVIE_BASE_URI}/search/movie`)
-            .query({ api_key: `${API_KEY_MOVIE}` })
-            .query({query: movieName})
-            .query({page: page})
-            .send();
-        const listMovies = response.body.results;
-        const movies = [];
-        for (let index = 0; index < listMovies.length; index++) {
-            const movie = {
-                vote_count: listMovies[index].vote_count,
-                vote_average: listMovies[index].vote_average,
-                title: listMovies[index].title,
-                popularity: listMovies[index].popularity,
-                original_language: listMovies[index].original_language,
-                original_title: listMovies[index].original_title,
-                overview: listMovies[index].overview,
-                release_date: listMovies[index].release_date,
-                id: listMovies[index].id
-            };
-            movies.push(movie);
-            
-        }
 
-        stream.write({ movies });
-        console.log("total pages: " + response.body.total_pages, "pages: " + pages)
-        if(pages !== response.body.total_pages){
-            pages++;
-            await searchMovieDbProvider(movieName, pages, stream);
-        }
-    } catch (error) {
-        //console.error(error)
+    let response = await request
+        .get(`${MOVIE_BASE_URI}/search/movie`)
+        .query({ api_key: `${API_KEY_MOVIE}` })
+        .query({query: movieName})
+        .query({page: page})
+        .send();
+    const listMovies = response.body.results;
+    const movies = [];
+    for (let index = 0; index < listMovies.length; index++) {
+        const movie = {
+            vote_count: listMovies[index].vote_count,
+            vote_average: listMovies[index].vote_average,
+            title: listMovies[index].title,
+            popularity: listMovies[index].popularity,
+            original_language: listMovies[index].original_language,
+            original_title: listMovies[index].original_title,
+            overview: listMovies[index].overview,
+            release_date: listMovies[index].release_date,
+            id: listMovies[index].id
+        };
+        movies.push(movie);
+        
+    }
+
+    stream.write({ movies });
+    console.log("total pages: " + response.body.total_pages, "pages: " + pages)
+    if(pages !== response.body.total_pages){
+        pages++;
+        await searchMovieDbProvider(movieName, pages, stream);
     }
 
     return _.flatten(movies);
--- src/test/api-test.spec.js
+++ src/test/api-test.spec.js
@@ -11,7 +11,15 @@ const client = caller('127.0.0.1:5053', PROTO_PATH, 'MovieService');
 describe('Testing Movies', async () => {
   it('Shold return all title with search name', async () => {
     const res = await client.searchMovie({ movieName: 'Bob', page: page })
-    let name = res.movies.filter((titleSearch) => {titleSearch.title.indexOf("Bob")});
-    expect(name === -1, "The title does not contain the searched word")
+    const movies = [];
+    res.on('data', (data) => {
+      movies.push(data);
+    })
+
+    res.on('end', () => {
+      let name = res.movies.filter((titleSearch) => {titleSearch.title.indexOf("Bob")});
+      expect(name === -1, "The title does not contain the searched word");
+      done();
+    });
   });
 });
