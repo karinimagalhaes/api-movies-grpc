40f0711e9c552fd307ba7ad7725d786630223561
Karini Magalhaes <karini.magalhaes@maxmilhas.com.br>
2019-06-17 10:39:39 -0300

    Alteração da busca

--- src/test/api-test.spec.js
+++ src/test/api-test.spec.js
@@ -10,8 +10,8 @@ const client = caller('127.0.0.1:5053', PROTO_PATH, 'MovieService');
 
 describe('Testing Movies', async () => {
   it('Shold return all title with search name', async () => {
-    const res = await client.searchMovie({ movieName: 'Bob', page: page })
-    let name = res.movies.filter((titleSearch) => {titleSearch.title.indexOf("Bob")});
+    const res = await client.searchMovie({ movieName: 'Vingadores', page: page })
+    let name = res.movies.filter((titleSearch) => {titleSearch.title.indexOf("Vingadores")});
     expect(name === -1, "The title does not contain the searched word")
   });
 });
